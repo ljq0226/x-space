e4104c7eeb5e8ec7b5684d810917ddfd169a36a6
ljq0226 <1035821043@qq.com>
2023-07-22 09:45:11 +0800

    chore:配置 tailwindcss

--- client/apps/web/app/page.tsx
+++ client/apps/web/app/page.tsx
@@ -1,9 +1,10 @@
 import { Button, Header } from "ui";
-
+import '../styles/globals.scss'
 export default function Page() {
   return (
     <>
       <Header text="Web" />
+      <div className="underline">asd</div>
       <Button />
     </>
   );
